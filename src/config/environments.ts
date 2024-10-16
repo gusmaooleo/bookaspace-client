1f561b9f0e680b31ec901ec603fb589641628e5d
Leonardo <leogusmaocf@gmail.com>
2024-10-16 00:30:38 -0300

    chore: add public env prefix

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -1,5 +1,5 @@
 const environments = {
-  url: process.env.API_URL || 'http://localhost:8080',
+  url: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
   username: process.env.USERNAME || 'myclientid',
   password: process.env.PASSWORD || 'myclientsecret' 
 }
